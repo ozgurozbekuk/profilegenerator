f86862a255f697d180bac93cfcd63d297a0da52c
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2023-06-01 14:26:12 +0300

    profile-genrator
    Random profile generator with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from "axios"
 import './App.css';
 
 function App() {
+
+  const [profileName,setProfileName] = useState("");
+  const [profileCell,setProfileCell] = useState("");
+  const [profileImage,setProfileImage] = useState("");
+  const [profileEmail,setProfileEmail] = useState("");
+
+ 
+
+  const profileData = async () => {
+    try {
+      const res = await axios.get('https://randomuser.me/api/')
+      console.log(res.data.results[0].name.first);
+      setProfileCell(res.data.results[0].cell);
+      setProfileEmail(res.data.results[0].email)
+      setProfileName(`${res.data.results[0].name.first} ${res.data.results[0].name.last}`);
+      setProfileImage(res.data.results[0].picture.large)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <img src={profileImage} />
+        <h1>{profileName}</h1>
+        <p>{profileEmail}</p>
+        <p>{profileCell}</p>
+        <button onClick={profileData}>New Profile</button>
+      </div>
     </div>
   );
 }
